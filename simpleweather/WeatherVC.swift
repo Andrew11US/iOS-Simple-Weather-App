d2eec0155c09fff86a41ce59f909f0687ef3b8d8
Andrii Halabuda <losAngelesBoy11@gmail.com>
2017-03-20 20:27:24 +0200

    Fixed No GPS Connection Crash

--- simpleweather/WeatherVC.swift
+++ simpleweather/WeatherVC.swift
@@ -55,12 +55,22 @@ class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource, C
         if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
             
             currentLocation = locationManager.location
-            Location.sharedInstance.latitude = currentLocation.coordinate.latitude
-            Location.sharedInstance.longitude = currentLocation.coordinate.longitude
             
-            currentWeather.downloadWeatherDetails {
-                self.downloadForecastData {
+            if locationManager.location == nil {
+            
+                locationLbl.text = "Unable to Position :("
+                currentWeatherImg.image = UIImage(named: "No")
+            
+                print("No GPS")
+            } else {
+            
+                Location.sharedInstance.latitude = currentLocation.coordinate.latitude
+                Location.sharedInstance.longitude = currentLocation.coordinate.longitude
+            
+                currentWeather.downloadWeatherDetails {
+                    self.downloadForecastData {
                     self.updateMainUI()
+                    }
                 }
             }
             
